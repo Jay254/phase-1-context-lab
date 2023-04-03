ee0aad8a9cb3bef7e4003bdbb6333bfcb23a3641
Jay254 <jyegon504@gmail.com>
2023-04-03 20:13:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,85 @@
 /* Your Code Here */
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(arr) {
+    return arr.map(function(row) {
+      return createEmployeeRecord.apply(null, row);
+    });
+  }
+  
+  function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+  
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date
+    });
+  
+    return this;
+  }
+  
+  function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+  
+    this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date
+    });
+  
+    return this;
+  }
+  
+  function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(function(event) {
+      return event.date === date;
+    });
+  
+    let timeOut = this.timeOutEvents.find(function(event) {
+      return event.date === date;
+    });
+  
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(date) {
+    let hours = hoursWorkedOnDate.call(this, date);
+    return hours * this.payPerHour;
+  }
+  
+  function allWagesFor() {
+    let eligibleDates = this.timeInEvents.map(function(event) {
+      return event.date;
+    });
+  
+    let payable = eligibleDates.reduce(function(memo, date) {
+      return memo + wagesEarnedOnDate.call(this, date);
+    }.bind(this), 0);
+  
+    return payable;
+  }
+  
+  function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function(record) {
+      return record.firstName === firstName;
+    });
+  }
+  
+  function calculatePayroll(records) {
+    return records.reduce(function(memo, record) {
+      return memo + allWagesFor.call(record);
+    }, 0);
+  }
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
